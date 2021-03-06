a2dee44fc75b9f1094324973b6e59c68e1627664
Mika Rinkinen <mirinkinen@gmail.com>
2021-03-06 16:30:05 +0200

    Consume messages

--- consumer/Program.cs
+++ consumer/Program.cs
@@ -1,14 +1,83 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 
 namespace consumer
 {
     class Program
     {
+        static string connectionString = "Endpoint=sb://sb-111.servicebus.windows.net/;SharedAccessKeyName=listen;SharedAccessKey=0yh7ESr7/jbppmc/pXV9coVWl8jPFQxhyQ831A9A4dc=;EntityPath=simple-queue";
+        static string queueName = "simple-queue";
+
         static void Main(string[] args)
         {
-            
+            try
+            {
+                if (args.Length != 2)
+                {
+                    System.Console.WriteLine("Give consumer name and delay between reads in milliseconds: consumer.exe consumerA 500");
+                    return;
+                }
 
-            Console.WriteLine("Hello World!");
+                var consumerName = args[0].ToString();
+                var readDelay = TimeSpan.FromMilliseconds(Convert.ToInt32(args[1].ToString()));
+
+                var cancellationTokenSource = new CancellationTokenSource();
+                var task = new Task(() => ConsumeMessages(consumerName, readDelay, cancellationTokenSource.Token));
+
+                System.Console.WriteLine("Press [enter] to stop consuming messages.");
+                task.Start();
+                System.Console.ReadLine();
+                System.Console.WriteLine("Cancellation requested.");
+                cancellationTokenSource.Cancel();
+                task.Wait();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private static async Task ConsumeMessages(string consumerName, TimeSpan readDelay, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await using (var client = new ServiceBusClient(connectionString))
+                {
+                    var options = new ServiceBusReceiverOptions
+                    {
+                        ReceiveMode = ServiceBusReceiveMode.PeekLock
+                    };
+
+                    ServiceBusReceiver receiver = client.CreateReceiver(queueName, options);
+                    System.Console.WriteLine("Ready to consume");
+
+                    while (true)
+                    {
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            return;
+                        }
+
+                        var message = await receiver.ReceiveMessageAsync();
+                        System.Console.WriteLine($"{consumerName} consuming: {message.Body.ToString()}");
+
+                        // Simulate work...
+                        await Task.Delay(readDelay);
+
+                        await receiver.CompleteMessageAsync(message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                System.Console.WriteLine("Message consuming stopped.");
+            }
         }
     }
 }
--- producer/Program.cs
+++ producer/Program.cs
@@ -5,8 +5,6 @@ using Azure.Messaging.ServiceBus;
 
 namespace producer
 {
-
-
     class Program
     {
         static string connectionString = "Endpoint=sb://sb-111.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=7+p4vTqrk9cf2BqY4HpaOkqrad5U8IdnC+vWj+pbC3k=;EntityPath=simple-queue";
@@ -14,25 +12,30 @@ namespace producer
 
         static void Main(string[] args)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var task = new Task(() => ProduceMessages(cancellationTokenSource.Token));
+            try
+            {
+                var cancellationTokenSource = new CancellationTokenSource();
+                var task = new Task(() => ProduceMessages(cancellationTokenSource.Token));
 
-            System.Console.WriteLine("Press [enter] to stop producing messages.");
-            task.Start();
-            System.Console.ReadLine();
-            System.Console.WriteLine("Cancellation requested.");
-            cancellationTokenSource.Cancel();
-            task.Wait();
+                System.Console.WriteLine("Press [enter] to stop producing messages.");
+                task.Start();
+                System.Console.ReadLine();
+                System.Console.WriteLine("Cancellation requested.");
+                cancellationTokenSource.Cancel();
+                task.Wait();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         private static async Task ProduceMessages(CancellationToken cancellationToken)
         {
             try
             {
-                // create a Service Bus client 
                 await using (var client = new ServiceBusClient(connectionString))
                 {
-                    // create a sender for the queue 
                     ServiceBusSender sender = client.CreateSender(queueName);
 
                     for (int i = 0; i < 100; i++)
@@ -46,9 +49,8 @@ namespace producer
                         System.Console.WriteLine($"Sending: {message}");
                         await sender.SendMessageAsync(new ServiceBusMessage(message), cancellationToken);
 
-                        await Task.Delay(500);
+                        await Task.Delay(100);
                     }
-
                 }
             }
             finally
